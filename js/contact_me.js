197b568fe4a2399278e37bfa50c130c0d952317f
rditto <30605019+rditto@users.noreply.github.com>
2017-08-02 00:05:18 -0500

    revert

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "russellditto2@gmail.com",
+                url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
